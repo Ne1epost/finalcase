bade5f61222716cbc968047e62abf99ae1dd2a68
Ne1epost <119726311+Ne1epost@users.noreply.github.com>
2023-02-18 13:23:05 +0300

    1st try

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿void ShowArray(string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+    string[] finalArray = {"hello", "22", "world", ":-)"};
+    string[] emptyArray = new string [4];
+
+
+    for (int i=0; i<finalArray.Length; i++)
+    {
+      if (finalArray[i].Length <= 3)
+     {
+      emptyArray[i] = finalArray[i];
+      }
+    }
+
+  ShowArray(emptyArray);
